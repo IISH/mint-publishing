b514c83327c90507ff8febf2b7ba5e2807a0503c
Kostas Sismanis <ksismanis@image.ece.ntua.gr>
2015-03-17 14:02:30 +0200

    create oai-dc elements when rdf is published

--- MintPIMongoOAIConsumers/src/gr/ntua/ivml/mint/pi/consumers/util/MongoUtils.java
+++ MintPIMongoOAIConsumers/src/gr/ntua/ivml/mint/pi/consumers/util/MongoUtils.java
@@ -9,16 +9,32 @@ public class MongoUtils {
 	
 	ThriftToJSON transformer;
 	BasicDBObject obj;
+	OAIDCGenerator dcgen;
+	BasicDBObject temp;
 	
 	public MongoUtils(){
 		transformer = new ThriftToJSON();
+		dcgen = new OAIDCGenerator();
 	}
 	
 	public void insertOAIRecord(ItemMessage msg, String collectionName){
 		obj = transformer.toJSON(msg);
+		BasicDBObject obj_copy = transformer.toJSON(msg);
+		
 		if(!recordExists(obj, collectionName)){
 			obj.put("isPublished", true);
 			MongoDB.getDB().getCollection(collectionName).insert(obj);
+
+			BasicDBObject  lala = (BasicDBObject) obj.get("namespace");
+			String prefix  = lala.getString("prefix");
+			if (prefix.equals("rdf")){
+			temp = dcgen.getOAIDCObject(obj_copy);
+			
+			if (temp != null){
+				MongoDB.getDB().getCollection(collectionName).insert(temp);
+				temp = null;
+			}
+		}
 		}else{
 			insertOAIDuplicateRecord(obj);
 		}
--- MintPIMongoOAIConsumers/src/gr/ntua/ivml/mint/pi/test/ConsumerTest.java
+++ MintPIMongoOAIConsumers/src/gr/ntua/ivml/mint/pi/test/ConsumerTest.java
@@ -1,5 +1,7 @@
 package gr.ntua.ivml.mint.pi.test;
 
+import java.util.Date;
+
 import gr.ntua.ivml.mint.pi.consumers.MessageConsumer;
 import gr.ntua.ivml.mint.pi.consumers.util.Config;
 
@@ -9,7 +11,9 @@ public class ConsumerTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//System.out.println(Config.get("queue.host"));
+		System.out.println(Config.get("queue.host"));
+		System.out.println(new Date());
+
 		for(int i = 0; i < 10;i++){
 			MessageConsumer con = new MessageConsumer();
 			Thread thread = new Thread(con, "consumer #"+i);
