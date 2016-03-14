70185e6a74240358ffd437550bbbadee6efc6156
costas <ksismanis@image.ntua.gr>
2016-03-14 15:18:40 +0200

    removing

--- MintPIThriftConsumers/src/gr/ntua/ivml/mint/pi/consumers/strategies/MessageConsumerStrategy.java
+++
@@ -1,7 +0,0 @@
-package gr.ntua.ivml.mint.pi.consumers.strategies;
-
-import gr.ntua.ivml.mint.pi.messages.ItemMessage;
-
-public interface MessageConsumerStrategy {
-	void consume(ItemMessage msg, String topic);
-}
